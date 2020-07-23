fd08c382062593f4c73a500194735e588c081d46
RussMasu <masuda3@hawaii.edu>
2020-07-22 17:30:36 -1000

    Updated feature gui

--- pepeiao/gui.py
+++ pepeiao/gui.py
@@ -8,22 +8,33 @@ class GraphicalUserInterface:
         # create window
         self.window = tk.Tk()
         # create widget
-        self.greeting = tk.Label(text="Input .wav files")
+        self.greeting = tk.Label(text="Input .wav files", width=20, height=2)#.grid(row=0, column=0)
         # add widget to window
         self.greeting.pack()
-        # create entry widget for user input
-        self.entry = tk.Entry()
-        self.entry.bind('<Return>', self.onReturn)
-        self.entry.pack()
+        # create text box for user input
+        self.text = tk.Text()#.grid(row=1, column=0)
+        self.text.bind('<Return>', self.onReturn)
+        self.text.pack()
+        # create submit button
+        self.button = tk.Button(text="Submit", width=10, height=2, command=self.onClick)#.grid(row=2, column=1)
+        self.button.pack(side="right")
         # required for window to be shown
         self.window.mainloop()
 
     def onReturn(self, event):
-        """When Return key is pressed in entry widget"""
-        self.var = self.entry.get()
-        print(self.var)
+        """When Return key is run pepeiao.feature with args taken from text box"""
+        self.var = self.text.get("1.0", tk.END)  # passing return
+        # parse user input
+        self.var = self.var[:-1]
+        #TODO load in different window
+        self.greeting.configure(text="Loading wav files. . .")
+        subprocess.run(["pepeiao", "feature", self.var])
 
+    def onClick(self):
+        self.var = self.text.get("1.0", tk.END)
+        # parse user input
+        self.var = self.var[:-1]
+        subprocess.run(["pepeiao", "feature", self.var])
 
-# app = GraphicalUserInterface()
-file = "C:/Users/Russ Masuda/PycharmProjects/birdNN/data/S4A01450_20170506_063300.wav"
-subprocess.run(["pepeiao", "feature", file])
+app = GraphicalUserInterface()
+#file = "C:/Users/Russ Masuda/PycharmProjects/birdNN/data/S4A01450_20170507_180200.wav"
