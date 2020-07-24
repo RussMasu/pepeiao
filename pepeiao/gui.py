4643eff928808204caa091d2917560e036bddfe4
RussMasu <masuda3@hawaii.edu>
2020-07-23 17:01:53 -1000

    added show frame function to change between frames

--- pepeiao/gui.py
+++ pepeiao/gui.py
@@ -1,42 +1,64 @@
 import tkinter as tk
 import subprocess
-import pepeiao.feature
 
-class GraphicalUserInterface:
-    def __init__(self):
-        """input window gui"""
-        self.var = None
-        self.window = tk.Tk()
+class GraphicalUserInterface(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        # self.current = None
+        self.frames = {}
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
 
-        self.greeting = tk.Label(text=".feat files are required to train the neural network.\n"
-                                      " Do you wish to load existing .feat files or create new .feat"
-                                      " files from .wav files? ", width=75, height=10)  # .grid(row=0, column=0
-        self.greeting.pack()
+        container.grid()
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-        self.button = tk.Button(text="Create", width=10, height=2, command=self.linkWindow)  # .grid(row=2, column=1)
-        self.button.pack(side="right")
-        self.window.mainloop()
+        # add all pages to self.frames
+        for f in (inputPage, featurePage):
+            frame = f(container, self)
+            self.frames[f] = frame
 
-    def feature(self):
-        """feature window gui"""
-        # create window
-        self.window = tk.Tk()
-        # create widget
-        self.greeting = tk.Label(text="Input .wav files", width=20, height=2)#.grid(row=0, column=0)
-        # add widget to window
-        self.greeting.pack()
-        # create text box for user input
-        self.text = tk.Text()#.grid(row=1, column=0)
-        self.text.bind('<Return>', self.onReturn)
-        self.text.pack()
-        # create submit button
-        self.button = tk.Button(text="Submit", width=10, height=2, command=self.onClick)#.grid(row=2, column=1)
-        self.button.pack(side="right")
-        # required for window to be shown
-        self.window.mainloop()
+        # show starting page on init
+        self.show_frame(inputPage)
 
+    def show_frame(self, page):
+        # remove all frames from grid
+        for frame in self.frames:
+            self.frames.get(frame).grid_remove()
+        # display page to grid
+        self.frames.get(page).grid()
+        # self.current = page
+
+
+class inputPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text=".feat files are required to train the neural network.\n"
+                                    "  Do you wish to load existing .feat "
+                                    "files or create new .feat files from .wav files?")
+        label.grid(row=0, column=0)
+
+        button = tk.Button(self, text="Create",
+                            command=lambda: controller.show_frame(featurePage))
+        button.grid(row=1, column=1)
+        #self.window.mainloop()
+
+
+class featurePage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Enter Feat Files")
+        label.grid(row=0, column=0)
+
+        #text = tk.Text()
+        text = tk.Entry()
+        text.grid(row=0, column=1)
+
+        button = tk.Button(self, text="<<Back",
+                            command=lambda: controller.show_frame(inputPage))
+        button.grid(row=1, column=1)
+"""
     def onReturn(self, event):
-        """When Return key is run pepeiao.feature with args taken from text box"""
+        #When Return key is run pepeiao.feature with args taken from text box
         self.var = self.text.get("1.0", tk.END)  # passing return
         # parse user input
         self.var = self.var[:-1]
@@ -54,6 +76,7 @@ class GraphicalUserInterface:
 
     def linkWindow(self):
         pass
-
+"""
 app = GraphicalUserInterface()
+app.mainloop()
 #file = "C:/Users/Russ Masuda/PycharmProjects/birdNN/data/S4A01450_20170507_180200.wav"
