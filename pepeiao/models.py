89d0d91982d4b91f3d0c0de4e0193a4c88374f05
RussMasu <masuda3@hawaii.edu>
2020-07-31 09:07:40 -1000

    Reverted transfer layer

--- pepeiao/models.py
+++ pepeiao/models.py
@@ -83,12 +83,10 @@ def transfer(input_shape):
     # create new model
     inputs = keras.Input(shape=(input_shape))
     x = base_model(inputs, training=False)
-    x = keras.layers.Conv2D(16, (2, 2), activation='relu')(x)
     x = keras.layers.GlobalAveragePooling2D()(x)
     outputs = keras.layers.Dense(1)(x)
     model = keras.Model(inputs, outputs, name="transfer")
     model.summary()
-    # must set shape manually or Dense dim not defined error
     model.compile(optimizer='rmsprop',
                   loss='binary_crossentropy',
                   metrics=['binary_accuracy', _prob_bird])
